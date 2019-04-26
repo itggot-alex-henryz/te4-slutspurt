52930c7048c827304fc355a70b94532415af5a72
itggot-alex-henryz <alex.henryz@itggot.se>
2019-04-26 13:09:00 +0200

    Created Base application

---
+++ LinkedList/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int x;
+    struct node *next
+};
+
+int main(){
+
+    return 0;
+}
--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 import psycopg2
-from flask import Flask, render_template, session, redirect, request
+from flask import Flask, render_template, session, redirect, request, url_for
 app = Flask(__name__)
 
-conn = psycopg2.connect(host="localhost",database="fruitdb", user="postgres", password="docker")
+conn = psycopg2.connect(host="localhost",database="slutspurt", user="postgres", password="docker")
 cur = conn.cursor()
 
 @app.before_first_request
@@ -19,21 +19,14 @@ def language_switch():
     return redirect("/")
 
 @app.route('/')
-def index():
-    return render_template("index_{}.html".format(session['lang']))
-
-@app.route('/fruits')
-def fruits():
-    cur.execute("SELECT * FROM fruits")
-    fruitlist = cur.fetchall()
-    return render_template("fruits_{}.html".format(session['lang']), fruits=fruitlist)
-
-@app.route('/fruit/<name>')
-def fruit(name=None):
-    cur.execute("SELECT * FROM fruits WHERE name='{}'".format(name))
-    fruitdata = cur.fetchone()
-    print(fruitdata)
-    return  render_template("fruit_{}.html".format(session['lang']), fruit=fruitdata)
+def home():
+    return render_template('index_{}.html'.format(session['lang']))
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return redirect('/')
+    elif request.method == 'GET':
+        return render_template('login_{}.html'.format(session['lang']))
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(24)
